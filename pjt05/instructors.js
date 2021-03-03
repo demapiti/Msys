056b79b1fca3f2dfb2ccbc2584b89a93c36a899e
dema <gabriel_bb_2007@hotmail.com>
2021-03-03 14:28:30 -0300

    2.1

--- pjt05/instructors.js
+++ pjt05/instructors.js
@@ -2,6 +2,11 @@ const fs = require('fs')
 const data = require('./data.json')
 const { age, date } = require('./utils')
 
+// index
+exports.index = (req, res) => {
+    return res.render('instructors/index', { instructors: data.instructors })
+}
+
 // show
 exports.show = (req, res) => {
     const { id } = req.params
@@ -71,4 +76,51 @@ exports.edit = (req, res) => {
     }
 
     return res.render('instructors/edit', { instructor: instructor })
+}
+
+// put
+exports.put = (req, res) => {
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find((instructor, foundIndex) => {
+        if (instructor.id == id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundInstructor) return res.send('Instructor not found!')
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+        if (err) return res.send('Write file error!')
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
+
+// delete
+exports.delete = (req, res) => {
+    const { id } = req.body
+
+    const filteredInstructors = data.instructors.filter((instructor) => {
+        return instructor.id != id
+    })
+
+    data.instructors = filteredInstructors
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+        if (err) return res.send('Write file error!')
+
+        return res.redirect('/instructors')
+    } )
 }
