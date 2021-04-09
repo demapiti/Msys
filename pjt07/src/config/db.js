3762331fd0e3f5d4ad75bd2fb9c6c45c26074511
demapiti <gabriel_bb_2007@hotmail.com>
2021-04-09 12:12:37 -0300

    2.9

---
+++ pjt07/src/config/db.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg')
+
+module.exports = new Pool ({
+    user: 'postgres',
+    password: 'dema8484',
+    host: 'localhost',
+    port: 5432,
+    database: 'ecommerce'
+})
