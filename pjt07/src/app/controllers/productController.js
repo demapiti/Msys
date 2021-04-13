a3273261a96a5be87a69e1ea2558b0db81b17bf7
dema <gabriel_bb_2007@hotmail.com>
2021-04-13 15:02:52 -0300

    3.0

--- pjt07/src/app/controllers/productController.js
+++ pjt07/src/app/controllers/productController.js
@@ -1,5 +1,27 @@
+const Category = require('../models/Category');
+const Product = require('../models/Product');
+
 module.exports = {
-    create(req, res) {
-        return res.render('products/create.njk')
+    async create(req, res) {
+        const results = await Category.all()
+        const categories = results.rows;
+
+        return res.render('products/create.njk', { categories })
+    },
+    async post(req, res) {
+        const keys = Object.keys(req.body)
+
+        for (let key of keys) {
+            if (req.body[key] == "")
+                return res.send('Por favor, preencha todos os campos.')
+        }
+
+        let results = await Product.create(req.body)
+        const product = results.rows[0].id
+
+        results = await Category.all()
+        const categories = results.rows
+
+        return res.render('products/create.njk', { categories, product })
     }
 }
