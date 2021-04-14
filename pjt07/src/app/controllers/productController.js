93e14f0e9ca375fab626b87265e19b41153f9bc8
dema <gabriel_bb_2007@hotmail.com>
2021-04-14 19:24:23 -0300

    3.1

--- pjt07/src/app/controllers/productController.js
+++ pjt07/src/app/controllers/productController.js
@@ -1,3 +1,5 @@
+const { formatPrice } = require('../../lib/utils');
+
 const Category = require('../models/Category');
 const Product = require('../models/Product');
 
@@ -17,11 +19,41 @@ module.exports = {
         }
 
         let results = await Product.create(req.body)
-        const product = results.rows[0].id
+        const productId = results.rows[0].id
+
+        return res.render(`products/${productId}/edit`)
+    },
+    async edit(req, res) {
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+
+        if (!product) return res.send("Product not found!")
+
+        product.old_price = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
 
         results = await Category.all()
         const categories = results.rows
 
-        return res.render('products/create.njk', { categories, product })
+        return res.render('products/edit.njk', { categories, product })
+    },
+    async put(req, res) {
+        const keys = Object.keys(req.body)
+
+        for (let key of keys) {
+            if (req.body[key] == "")
+                return res.send('Por favor, preencha todos os campos.')
+        }
+
+        req.body.price = req.body.price.replace(/\D/g,"")
+
+        if (req.body.old_price != req.body.price) {
+            const oldProduct = await Product.find(req.body.id)
+            req.body.old_price = oldProduct.rows[0].price
+        }
+
+        await Product.update(req.body)
+
+        return res.redirect(`/products/${req.body.id}/edit`)
     }
 }
