a3f0955b92ef16ad970020523146d93dc26207c1
demapiti <gabriel_bb_2007@hotmail.com>
2021-04-15 12:01:50 -0300

    3.2

--- pjt07/src/app/controllers/productController.js
+++ pjt07/src/app/controllers/productController.js
@@ -2,6 +2,7 @@ const { formatPrice } = require('../../lib/utils');
 
 const Category = require('../models/Category');
 const Product = require('../models/Product');
+const File = require('../models/File');
 
 module.exports = {
     async create(req, res) {
@@ -18,8 +19,14 @@ module.exports = {
                 return res.send('Por favor, preencha todos os campos.')
         }
 
+        if (req.files.length == 0)
+            return res.send('Por favor, envie pelo menos uma imagem.')
+
         let results = await Product.create(req.body)
         const productId = results.rows[0].id
+
+        const filesPromise = req.files.map((file) =>  File.create({...file, product_id: productId}))
+        await Promise.all(filesPromise)
 
         return res.render(`products/${productId}/edit`)
     },
@@ -35,14 +42,32 @@ module.exports = {
         results = await Category.all()
         const categories = results.rows
 
-        return res.render('products/edit.njk', { categories, product })
+        results = await Product.files(product.id)
+        let files = results.rows
+        files = files.map((file) => ({
+            ...file,
+            src: `${req.protocol}://${req.headers.host}${file.path.replace('public',"")}`
+        }))
+
+        return res.render('products/edit.njk', { categories, product, files })
     },
     async put(req, res) {
         const keys = Object.keys(req.body)
 
         for (let key of keys) {
-            if (req.body[key] == "")
+            if (req.body[key] == "" && key != "removed_files")
                 return res.send('Por favor, preencha todos os campos.')
+        }
+
+        if (req.body.removed_files) {
+            // 1,2,3,
+            const removedFiles = req.body.removed_files.split(',') // [1,2,3,]
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1) // [1,2,3]
+
+            const removedFilesPromise = removedFiles.map((file) => File.delete(id))
+
+            await Promise.all(removedFilesPromise)
         }
 
         req.body.price = req.body.price.replace(/\D/g,"")
--- pjt07/src/app/models/Product.js
+++ pjt07/src/app/models/Product.js
@@ -23,7 +23,7 @@ module.exports = {
         return db.query(query, values)
     },
     find(id) {
-        return db.query('SELECT * FROM products WHERE id = $1', [id])
+        return db.query(`SELECT * FROM products WHERE id = $1`, [id])
     },
     update(data) {
         const query = `
@@ -44,5 +44,11 @@ module.exports = {
         ]
 
         return db.query(query, values)
+    },
+    delete(id) {
+        return db.query('DELETE FROM products WHERE id = $1', [id])
+    },
+    files(id) {
+        return db.query(`SELECT * FROM files WHERE product_id = $1`, [id])
     }
 }
